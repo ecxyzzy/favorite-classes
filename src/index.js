a740fd97b67d570d92c9d879a9f87b842a6bebd8
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-02-14 13:24:49 -0800

    Implement Home and Class components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '', classes: []};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.state.classes.push(this.state.value);
+    this.setState({value: '', classes: this.state.classes});
+  }
+
+  render() {
+    return (
+      <>
+      <h1>Favorite Classes</h1>
+      <ul>
+        {this.state.classes.map((c) => <Class name={c} />)}
+      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
+      </>
+    )
+  }
+}
+
+class Class extends React.Component {
+  render() {
+    return (
+      <li>{this.props.name}</li>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
