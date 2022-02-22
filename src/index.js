0f6ad42233d53908d45a2a99e15323b18205975d
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-02-21 16:01:04 -0800

    Implement PeterPortal API functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import fetch from 'cross-fetch';
 import './index.css';
+
+const PETERPORTAL_REST_URL = 'https://api.peterportal.org/rest/v0'
+const PETERPORTAL_GQL_URL = 'https://api.peterportal.org/graphql'
+const DEPARTMENT_ALIASES = {
+  'COMPSCI': /^CS/gm,
+  'I&CSCI': /^ICS/gm,
+  'IN4MATX': /^INF/gm
+}
 
 class Home extends React.Component {
   constructor(props) {
@@ -27,22 +36,70 @@ class Home extends React.Component {
       <ul>
         {this.state.classes.map((c) => <Class name={c} />)}
       </ul>
-      <h2>Add your favorite classes below!</h2>
+      <h1>Add your favorite classes below!</h1>
       <form onSubmit={this.handleSubmit}>
-        <label>Class Name: </label>
-        <input type="text" value={this.state.value} onChange={this.handleChange} />
-        <input class="submit" type="submit" value="Submit" />
+        <label>Class ID: </label>
+        <input type='text'
+        value={this.state.value}
+        onChange={this.handleChange}
+        title="Enter the class ID (case insensitive) here.
+        Shortened department names are fine (CS161 will resolve to COMPSCI161),
+        as are spaces (I&amp;C SCI 33 will resolve to I&amp;CSCI33)." />
+        <input className='submit' type='submit' value='Submit' />
       </form>
       </>
-    )
+    );
   }
 }
 
 class Class extends React.Component {
+  constructor(props) {
+    super(props);
+    let sanitizedName = this.props.name.replaceAll(' ', '').toUpperCase();
+    Object.keys(DEPARTMENT_ALIASES).map(
+      (k) => (sanitizedName = sanitizedName.replace(DEPARTMENT_ALIASES[k], k))
+    );
+    this.state = {sanitizedName: sanitizedName, restRes: null, instructors: null};
+  }
+
+  async componentDidMount() {
+    const restRes = await fetch(
+      `${PETERPORTAL_REST_URL}/courses/${this.state.sanitizedName}`,
+      {'method': 'GET'}).then(res => res.json());
+    const instructors = await fetch(
+      PETERPORTAL_GQL_URL, {
+        'method': 'POST',
+        'body': JSON.stringify({query: `
+          query {
+            course(id: "${this.state.sanitizedName}") {
+              instructor_history {
+                name
+              }
+            }
+          }
+        `}),
+        'headers' : {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(res => res.json());
+    this.setState({
+      sanitizedName: this.state.sanitizedName,
+      restRes: restRes,
+      instructors: instructors.data.course.instructor_history.map((i) => i == null ? 'Unknown' : i.name)
+    });
+  }
+  
   render() {
-    return (
-      <li>{this.props.name}</li>
-    )
+    return this.state.restRes == null ? null : (
+      <>
+        <h2>{this.state.restRes.department} {this.state.restRes.number}: {this.state.restRes.title}</h2>
+        <h3>Past instructors:</h3>
+        <ul>
+        {this.state.instructors.map((i) => <li>{i}</li>)}
+        </ul>
+      </>
+    );
   }
 }
 
